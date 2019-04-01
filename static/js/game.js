340c7c5db428c426a5f4e0c847b4271e9a9bab1a
adamRajmuller <rajfta@gmail.com>
2019-04-01 16:49:38 +0200

    updated Cards() to Card() (unnecessary)

--- static/js/game.js
+++ static/js/game.js
@@ -1,39 +1,50 @@
-function Player(hp, role, position, range, features) {
+let fullDeck = {
+    'bang': 5,
+    'missed': 5
+};
+
+
+
+function Player(hp, role, position, range, features, cards) { //cards: dictionary
     this.hp = hp;
     this.role = role;
     this.range = range;
-    this.hand = new Cards();
+    this.position = position;
+    this.hand = cards;
     this.features = features;
 
     this.bang = function(target) {
-        target.hp -= 1;
-        this.hand.removeCard('bang');
-    }
-}
-
-
-function Cards() {
-    this.values = {
-        'bang' : 2,
-        'missed' : 2
+        if (this.hand.bang > 0) {
+            target.hp -= 1;
+            this.removeCard('bang');
+        }
     };
-
     this.removeCard = function(card) {
-        this.values[card] -= 1;
+        this.hand[card] -= 1;
     };
-    this.shoot = function (target) {
+
+    this.draw2FromDeck = function() {
 
     }
 }
 
 
-let raj = new Player(4, "Scheriff", 2, []);
-let dombi = new Player(4, "Scheriff", 2, []);
+function Card(name, frontimage, backimage, visible) {
+    this.name = name;
+    this.frontside = frontimage;
+    this.backside = backimage;
+    this.visible = visible;
+}
 
-alert(dombi.hp );
-alert(raj.hand.values['bang']);
+
+let bang2miss2 = {'bang': 2, 'missed': 2};
+let raj =  new Player(4, "Scheriff", 2, 1, 4, bang2miss2);
+let dombi = new Player(4, "Scheriff", 2, [], 4, bang2miss2);
+
+alert('dombi hp: ' + dombi.hp );
+alert('raj bangs: ' + raj.hand['bang']);
 
 raj.bang(dombi);
 
-alert(dombi.hp );
-alert(raj.hand.values['bang']);
+alert('dombi hp: ' + dombi.hp );
+alert('raj bangs: ' + raj.hand['bang']);
