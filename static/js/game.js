bd9725afb6e57908ee0b57d4f32642f6567784b3
balazsDombovari <dbalazs1219@gmail.com>
2019-04-01 15:22:21 +0200

    Cards() Players()

--- static/js/game.js
+++ static/js/game.js
@@ -1,25 +1,39 @@
-function Player(hp, role, position, range, hand, features) {
+function Player(hp, role, position, range, features) {
     this.hp = hp;
     this.role = role;
     this.range = range;
-    this.hand = hand;
+    this.hand = new Cards();
     this.features = features;
-    this.bang = function(target) {}
-}
 
-let raj = new Player(4, "Scheriff", 2, [], []);
-
-
-function Cards() {
-    this.bang = 2;
-    this.missed = 2;
-
-    this.removeCard = function(card) {
-        this.hand[card]
+    this.bang = function(target) {
+        target.hp -= 1;
+        this.hand.removeCard('bang');
     }
 }
 
-let rajHand = new Cards();
-alert(rajHand.bang);
-rajHand.removeCard('bang');
-alert(rajHand.bang);
+
+function Cards() {
+    this.values = {
+        'bang' : 2,
+        'missed' : 2
+    };
+
+    this.removeCard = function(card) {
+        this.values[card] -= 1;
+    };
+    this.shoot = function (target) {
+
+    }
+}
+
+
+let raj = new Player(4, "Scheriff", 2, []);
+let dombi = new Player(4, "Scheriff", 2, []);
+
+alert(dombi.hp );
+alert(raj.hand.values['bang']);
+
+raj.bang(dombi);
+
+alert(dombi.hp );
+alert(raj.hand.values['bang']);
