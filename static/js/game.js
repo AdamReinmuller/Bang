29d2b92fb5c8b2e474dbcea4934a474ab79346cb
adamRajmuller <rajfta@gmail.com>
2019-04-02 14:02:38 +0200

    Player class Cards class refactor

--- static/js/game.js
+++ static/js/game.js
@@ -31,12 +31,12 @@ class Player { //cards: dictionary
         this.role = role;
         this.range = range;
         this.position = position;
-        this.hand = cards;
+        this.hand = new Cards(cards);
         this.features = features;
     }
 
     bang(target) {
-        if (this.hand.bang > 0 && target.hand.missed === 0) {
+        if (this.hand.cardsObject.bang > 0) {
             target.hp -= 1;
             this.removeCard('bang');
         }
@@ -50,7 +50,7 @@ class Player { //cards: dictionary
     }
 
     removeCard(card) {
-        this.hand[card] -= 1;
+        this.hand.cardsObject[card] -= 1;
     };
 
     draw2FromDeck() {
@@ -61,15 +61,15 @@ class Player { //cards: dictionary
 
 class Cards {
     constructor(object) {
-        this.items = object;
+        this.cardsObject = object;
         this.cardsArray = this.dictToArray();
         this.frontSide = this.getFrontside(); //gives back an array of the links
     }
 
     dictToArray() {
         let fArray = [];
-        let keys = Object.keys(this.items);
-        let values = Object.values(this.items);
+        let keys = Object.keys(this.cardsObject);
+        let values = Object.values(this.cardsObject);
         for (let i = 0; i < keys.length; i++) {
             for (let num = 0; num < values[i]; num++) {
                 fArray.push(keys[i])
@@ -86,7 +86,6 @@ class Cards {
             } else if (item === 'missed') {
                 frontSideArray.push('static/images/miss.png');
             }
-            else if(item === 'missed')
         }
         return frontSideArray
     }
@@ -99,14 +98,14 @@ let bang2miss2 = {'bang': 2, 'missed': 2};
 let raj = new Player(4, "Scheriff", 2, 1, 4, bang2miss2);
 let dombi = new Player(4, "Scheriff", 2, [], 4, bang2miss2);
 
-// alert('dombi hp: ' + dombi.hp );
-// alert('raj bangs: ' + raj.hand['bang']);
-//
-// raj.bang(dombi);
-//
-// alert('dombi hp: ' + dombi.hp );
-// alert('raj bangs: ' + raj.hand['bang']);
-let asdasd = new Cards(bang2miss2);
-for (frontside of asdasd.frontSide) {
-    alert(frontside)
-}
+alert('dombi hp: ' + dombi.hp );
+alert('raj bangs: ' + raj.hand.cardsObject['bang']);
+
+raj.bang(dombi);
+
+alert('dombi hp: ' + dombi.hp );
+alert('raj bangs: ' + raj.hand.cardsObject['bang']);
+// let asdasd = new Cards(bang2miss2);
+// for (frontside of asdasd.frontSide) {
+//     alert(frontside)
+// }
